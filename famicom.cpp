99feeee73a81b60c773f81585294aa99453011f8
org <ogamespec@gmail.com>
2025-01-17 14:41:50 +0300

    license header

--- famicom.cpp
+++ famicom.cpp
@@ -1,4 +1,23 @@
-﻿// https://github.com/emu-russia/breaks/blob/master/BreakingNESWiki_DeepL/MB/Famicom.md
+﻿/*
+ * breakscore - Famicom functional simulator.
+ *
+ * Copyright (C) 2024 org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// https://github.com/emu-russia/breaks/blob/master/BreakingNESWiki_DeepL/MB/Famicom.md
 
 // Instead of the signal designations /OE1,2 adopted in the nesdev.com community, we use the official names of these signals /RDP0,1
 
--- nrom.cpp
+++ nrom.cpp
@@ -1,15 +1,28 @@
+/*
+ * breakscore - Famicom functional simulator.
+ *
+ * Copyright (C) 2024 org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Simple Mapper Simulator (0) - NROM.
 // I'm not very knowledgeable about the zoo of their implementations, so this module will gradually be improved as new knowledge becomes available.
 
 #include "pch.h"
 
 using namespace BaseLogic;
-
-#define NROM_CATEGORY "NROM"
-#define CHR_ROM_NAME "CHR-ROM"
-
-// TBD: Change the CHR-ROM entity to just CHR
-// TBD: Add PRG dump
 
 namespace Mappers
 {
